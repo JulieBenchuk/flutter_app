284041129dfbcb1b4cb8ca6d296af38737ecead3
JulieBenchuk <juliebenchuk@gmail.com>
2023-05-28 11:46:58 +0300

    refactor: changed appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container(
-      margin: EdgeInsets.symmetric(vertical: 50),
-      color: Colors.pink,
-      padding: EdgeInsets.all(50),
-      child: Column(
-        children: <Widget> [
-          Expanded(child: Container(color: Colors.lightBlue,), flex: 1,),
-          Expanded(child: Container(color: Colors.green,), flex: 4),
-          Expanded(child: Container(color: Colors.yellow,), flex: 5),
-        ],
-      )));
+  runApp(const CryptoCurrencyListAPP());
+}
+
+class CryptoCurrencyListAPP extends StatelessWidget {
+  const CryptoCurrencyListAPP({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Crypto Currency App",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black87,
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          "Home Page",
+          textAlign: TextAlign.center,
+          textDirection: TextDirection.ltr,
+        )),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Some text in center"),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
